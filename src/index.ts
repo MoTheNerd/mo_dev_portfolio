4071a03dfaa3fcd0e58e6010964572fee93825a3
Mohammad Al-Ahdal <mo@mohammad.dev>
2019-10-05 14:45:18 -0600

    forgot this part 😅

--- src/index.ts
+++ src/index.ts
@@ -75,7 +75,7 @@ app.post("/post", async (req: express.Request, res: express.Response) => {
         if (await checkTokenAuthenticatedWithAuthServer(req.body.token)) {
             let putObjReq: PutObjectRequest
             var base64Data = req.body.post.imageData.replace(/^data:image\/png;base64,/, "").replace(/^data:image\/jpeg;base64,/, "");
-            let filename = `portfolio_thumbnails/${crypto.randomBytes(2).toString('hex')}_${moment.utc().toISOString()}_${req.body.filename}`
+            let filename = `portfolio_thumbnails/${crypto.randomBytes(2).toString('hex')}_${moment.utc().toISOString()}_${req.body.post.filename}`
             putObjReq = {
                 Bucket: 'modev',
                 Key: `${filename}`,
